0f4d749067be882eda95e3e98df86757426a6c63
matt-winkler <matt.winkler@dbtlabs.com>
2025-03-21 10:34:05 -0600

    update imports

--- nbdbt/dbt_cellmagic.py
+++ nbdbt/dbt_cellmagic.py
@@ -21,7 +21,8 @@ NBDBT_DEBUG = False
 from fastcore.all import patch
 
 # Internal Cell
-from dbt.cli.main import parse_args, adapter_management
+from dbt.adapters.factory import adapter_management
+from dbt.parser import parse_args
 from dbt.task.compile import CompileTask
 from dbt.contracts.results import RunExecutionResult
 import dbt.flags
